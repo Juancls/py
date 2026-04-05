d6efa42901c56daf0103f832a313c025d1af6d31
juan <juanceciliosilva@gmail.com>
2026-04-04 22:35:54 -0300

    Atualização para o bloco de escolher a nota do aluno seja encerrada ao ler uma string vazia, permitindo mais de uma consulta

--- exDicionario.py
+++ exDicionario.py
@@ -10,15 +10,25 @@ while True:
                    float(input(f"Nota segunda avaliação: "))]  # cadastra o nome e coloca duas notas para o mesmo
 
 
-print( "Deseja ver a nota de qual aluno: ")
-for nome in notas.keys(): #para cada nome contido nas keys(nomes) do dicionario irá executar algo
-    print(f"- {nome}") #printa os nomes
-escolha = input("")
-if escolha in notas: #verifica se o nomes escolhido esta no dicionario
-    print(f"As notas do aluno {escolha} são: ")
-    for i in range(2):
-        print(notas[escolha][i]) #print de todas as notas
-    print(f"A media do {escolha} é: ")
-    print((notas[escolha][0]+notas[escolha][1])/2) #print com a media das notas
-else:
-    print(f"Nenhum aluno com o nome {escolha} cadastrado")
+
+while True:
+
+    print("Deseja ver a nota de qual aluno: ")
+    for nome in notas.keys():  # para cada nome contido nas keys(nomes) do dicionario irá executar algo
+        print(f"- {nome}")  # printa os nomes
+
+    escolha = input("")
+    if escolha == '':
+        break
+
+    if escolha in notas:  # verifica se o nomes escolhido esta no dicionario
+        print(f"As notas do aluno {escolha} são: ")
+        for i in range(2):
+            print(notas[escolha][i])  # print de todas as notas
+            print(f"A media do {escolha} é: ")
+            print((notas[escolha][0] + notas[escolha][1]) / 2)  # print com a media das notas
+    else:
+        print(f"Nenhum aluno com o nome {escolha} cadastrado")
+
+
+print("Sessão finalizada")
