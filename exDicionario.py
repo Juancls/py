4b4770b3c4da10136545893b25450b29f9c31a1a
Victor-Lis <victorlisbronzo1@gmail.com>
2026-04-07 18:46:02 -0300

    fix: remove duplicate logs for student grades

--- exDicionario.py
+++ exDicionario.py
@@ -2,18 +2,15 @@ notas = {}
 
 print("Cadastro de notas de alunos")
 while True:
-    nome = input("Nome aluno: ")  # guarda o nome
+    nome = input("\nNome aluno: ")  # guarda o nome
     if nome == '': #verifica se a entrada foi vazio, se for, o codigo encerra e pula para o proximo bloco
         break
     else:
         notas[nome] = [float(input(f"Nota primeira avaliação: ")),
                    float(input(f"Nota segunda avaliação: "))]  # cadastra o nome e coloca duas notas para o mesmo
 
-
-
 while True:
-
-    print("Deseja ver a nota de qual aluno: ")
+    print("\nDeseja ver a nota de qual aluno: ")
     for nome in notas.keys():  # para cada nome contido nas keys(nomes) do dicionario irá executar algo
         print(f"- {nome}")  # printa os nomes
 
@@ -25,8 +22,8 @@ while True:
         print(f"As notas do aluno {escolha} são: ")
         for i in range(2):
             print(notas[escolha][i])  # print de todas as notas
-            print(f"A media do {escolha} é: ")
-            print((notas[escolha][0] + notas[escolha][1]) / 2)  # print com a media das notas
+        print(f"A media do {escolha} é: ")
+        print((notas[escolha][0] + notas[escolha][1]) / 2)  # print com a media das notas
     else:
         print(f"Nenhum aluno com o nome {escolha} cadastrado")
 
